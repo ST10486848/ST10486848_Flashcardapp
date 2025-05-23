5e230a1648609c729eaaab4274611cb73b90b343
larry <ST10486848@vcconnect.edu.za>
2025-05-24 01:34:00 +0200

    First commit

---
+++ app/src/main/java/com/example/st10486848_flashcardapp/Flashcardscreen.kt
@@ -0,0 +1,101 @@
+package com.example.st10486848_flashcardapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
+
+class Flashcardscreen : AppCompatActivity() {
+    var Questions: Array<String> = arrayOf<String>(
+        "The first world war started in 1914",
+        "The second world war started in 1945",
+        "Nelson Mandela was the first president of South Africa",
+        "Vlodymyr Zelensky is the current president of Ukraine",
+        "Joe Biden was the last 2024 president of America"
+    )
+    var Answers :Array<String> =arrayOf<String>("Correct","Incorrect","Correct","Correct","Incorrect")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_flashcardscreen)
+        // Declarations
+        val QuestionsTextView= findViewById<TextView>(R.id.QuestionsTextView)
+        val TrueButton = findViewById<Button>(R.id.TrueButton)
+        val FalseButton = findViewById<Button>(R.id.FalseButton)
+        val NextButton = findViewById<Button>(R.id.NextButton)
+        val FeedbackTextView = findViewById<TextView>(R.id.FeedbackTextView)
+        val TerminateButton = findViewById<Button>(R.id.TerminateButton)
+        // adding the code to displays the questions and prompt the user to answer
+        for(counter in 0..Questions.count()){
+            Questions += "${Questions[counter]}\n"
+            QuestionsTextView.text = Questions[counter]
+            TrueButton.setOnClickListener{
+                FeedbackTextView.text= "Correct"
+            }
+            FalseButton.setOnClickListener{
+                FeedbackTextView.text = "Incorrect"
+            }
+            NextButton.setOnClickListener {
+                Questions += "${Questions[counter]}\n"
+                QuestionsTextView.text = Questions[counter + 1]
+                FeedbackTextView.text = "Feedback"
+                TrueButton.setOnClickListener{
+                    FeedbackTextView.text = "Incorrect"
+                }
+                FalseButton.setOnClickListener{
+                    FeedbackTextView.text= "Correct"
+                }
+                NextButton.setOnClickListener{
+                    Questions += "${Questions[counter]}\n"
+                    QuestionsTextView.text = Questions[counter + 2]
+                    FeedbackTextView.text = "Feedback"
+                    TrueButton.setOnClickListener{
+                        FeedbackTextView.text= "Correct"
+                    }
+                    FalseButton.setOnClickListener{
+                        FeedbackTextView.text = "Incorrect"
+                    }
+                    NextButton.setOnClickListener{
+                        Questions += "${Questions[counter]}\n"
+                        QuestionsTextView.text = Questions[counter + 3]
+                        FeedbackTextView.text = "Feedback"
+                        TrueButton.setOnClickListener{
+                            FeedbackTextView.text= "Correct"
+                        }
+                        FalseButton.setOnClickListener{
+                            FeedbackTextView.text = "Incorrect"
+                        }
+                        NextButton.setOnClickListener{
+                            Questions += "${Questions[counter]}\n"
+                            QuestionsTextView.text = Questions[counter + 4]
+                            FeedbackTextView.text = "Feedback"
+                            TrueButton.setOnClickListener{
+                                FeedbackTextView.text = "Incorrect"
+                            }
+                            FalseButton.setOnClickListener{
+                                FeedbackTextView.text= "Correct"
+                            }
+                        }
+                    }
+                }
+            }
+            // Adding the code to open the last screen
+            TerminateButton.setOnClickListener{
+                val intent = Intent(this,Scorescreen::class.java)
+                // start the activity
+                startActivity(intent)
+            }
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}}
---
+++ app/src/main/java/com/example/st10486848_flashcardapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.st10486848_flashcardapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
+
+class MainActivity : AppCompatActivity() {
+    val Welcome = arrayOf<String>(
+        "Welcome to hystory quiz",
+        "click on start to begin the quiz", "you will be asked 5 questions about hystory",
+        "each questions is worth 1 point",
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val WelcomeTextView = findViewById<TextView>(R.id.WelcomeTextView)
+        // Adding our messages inside the WelcomeTextView
+        WelcomeTextView.text =Arrays.toString(Welcome)
+        val StartButton=findViewById<Button>(R.id.StartButton)
+        // Adding the code to open the second screen
+        StartButton.setOnClickListener {
+            val intent = Intent(this,Flashcardscreen::class.java)
+            // start the acivity
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+
+
+
